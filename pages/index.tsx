5c8a444d7d880cf3982bd493e342a841bd822643
SUKE2-ZUKIN <mono.316b@gmail.com>
2022-12-13 06:06:37 +0900

    GH-1: Fix default loading

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { GetServerSideProps, NextPage } from 'next';
 import { useState } from 'react';
-import styles from '../styles/Home.module.css'
+import 'semantic-ui-css/semantic.min.css';
+import { Loader } from "semantic-ui-react";
 
 interface SearchImage {
   id: string;
@@ -11,20 +10,27 @@ interface SearchImage {
   height: number;
 }
 
-const Home: NextPage = () => {
-  const [imgUrl, setImgUrl] = useState("");
+interface IndexPageProps {
+  initialImageUrl: string;
+}
 
-  const getRandomPicture = async (): Promise<SearchImage> => {
-    const res = await fetch(`https://api.thecatapi.com/v1/images/search`);
-    const result = await res.json();
-    console.log(result[0]);
-    return result[0];
-  };
+const getRandomPicture = async (): Promise<SearchImage> => {
+  const res = await fetch(`https://api.thecatapi.com/v1/images/search`);
+  const result = await res.json();
+  console.log(result[0]);
+  return result[0];
+};
+
+const Home: NextPage<IndexPageProps> = ({ initialImageUrl }) => {
+  const [imgUrl, setImgUrl] = useState(initialImageUrl);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleClick = async () => {
+    setIsLoading(true);
     const image = await getRandomPicture();
     console.log(image.url);
     setImgUrl(image.url);
+    setIsLoading(false);
   }
 
   return (
@@ -36,11 +42,15 @@ const Home: NextPage = () => {
       height: "100vh",
     }}>
       <h1>ランダム画像出力アプリ</h1>
-      <img 
-        src={imgUrl}
-        width={500}
-        height="auto"
-      />
+      {isLoading ? (
+        <Loader active size='huge' inline="centered" />
+      ) : (
+        <img 
+          src={imgUrl}
+          width={500}
+          height="auto"
+        />
+      )}
       <button 
         style={{
           marginTop: 18
@@ -49,5 +59,17 @@ const Home: NextPage = () => {
     </div>
   )
 }
+
+// SSR
+export const getServerSideProps: GetServerSideProps<
+  IndexPageProps
+> = async () => {
+  const img = await getRandomPicture();
+  return {
+    props: {
+      initialImageUrl: img.url,
+    }
+  };
+};
 
 export default Home
